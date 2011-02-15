7a336060125123909cad198abcfea862129e2714
eric <eric@moishe.(none)>
2011-02-14 18:48:09 -0800

    more prmtop

--- Prmtop.hs
+++ Prmtop.hs
@@ -6,11 +6,14 @@ import qualified Data.Map as Map
 import Data.List
 import Data.List.Split
 
-readPrmtopFile :: String -> [[String]]
+readPrmtopFile :: String -> [(String,[String])]
 readPrmtopFile input = 
-        let     alllines = lines input :: [String]
-                sections = splitWhen (not . isFlag) alllines
+        let     alllines = snd $ break isFlag $ lines input
+                splitSections input =
+                        (fst $ s) : fst $ break isFlag s
+                            where s = span isFlag input
         in sections
 
 isFlag :: String -> Bool
 isFlag x = isPrefixOf "%FLAG" x
+
